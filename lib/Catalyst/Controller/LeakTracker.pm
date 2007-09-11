2dc3001515ed83463ab57ae26bb277268e1bbf65
Yuval Kogman <nothingmuch@woobling.org>
2007-09-11 14:42:58 +0000

    stack dumps

--- lib/Catalyst/Controller/LeakTracker.pm
+++ lib/Catalyst/Controller/LeakTracker.pm
@@ -84,19 +84,155 @@ sub list_requests : Local {
     $c->res->content_type("text/html");
 }
 
-sub request : Local {
-    my ( $self, $c, $id ) = @_;
+sub object : Local {
+    my ( $self, $c, $request_id, $id ) = @_;
 
     my $log = $c->devel_events_log;
 
     my $trackers = $c->object_tracker_hash;
 
-    my @events = $log->limit( from => { request_id => $id }, to => "request_end" );
+    my @events = $log->limit( from => { request_id => $request_id }, to => { id => $id } );
+
+    my @stack;
+    foreach my $event ( @events ) {
+        my ( $type, %data ) = @$event;
+
+        if ( $type eq 'enter_action' ) {
+            push @stack, \%data;
+        } elsif ( $type eq 'leave_action' ) {
+            pop @stack;
+        }
+    }
+
+    my ( undef, %obj_event ) = @{ $events[-1] };
+
+    my $stack_dump = join("\n",
+        "$obj_event{file} line $obj_event{line} (package $obj_event{package})",
+        map {"  in action $_->{action_name} (controller $_->{class})" } reverse @stack
+    );
+
+    my %objs_by_id = map { $_->{id} => $_->{object} } values %{ $trackers->{$request_id}->live_objects };
+
+    my $obj = $objs_by_id{$id};
 
     use Data::Dumper;
-    my $log_output = Dumper(@events);
+    my $obj_dump = Dumper($obj);
 
-    my $leaks = Dumper(values %{ $trackers->{$id}->live_objects });
+    my @diags;
+    my $cycle_no;
+
+    # Callback function that is called once for each memory cycle found.
+    my $callback = sub {
+        my $path = shift;
+        $cycle_no++;
+        push( @diags, "Cycle #$cycle_no" );
+        foreach (@$path) {
+            my ($type,$index,$ref,$value) = @$_;
+
+            my $str = 'Unknown! This should never happen!';
+            my $refdisp = _ref_shortname( $ref );
+            my $valuedisp = _ref_shortname( $value );
+
+            $str = sprintf( '    %s => %s', $refdisp, $valuedisp )               if $type eq 'SCALAR';
+            $str = sprintf( '    %s => %s', "${refdisp}->[$index]", $valuedisp ) if $type eq 'ARRAY';
+            $str = sprintf( '    %s => %s', "${refdisp}->{$index}", $valuedisp ) if $type eq 'HASH';
+            $str = sprintf( '    closure %s => %s', "${refdisp}, $index", $valuedisp ) if $type eq 'CODE';
+
+            push( @diags, $str );
+        }
+    };
+
+    use Devel::Cycle;
+    Devel::Cycle::find_cycle( $obj, $callback );
+
+    my $cycles = join("\n", @diags);
+
+    $c->response->content_type("text/html");
+    $c->response->body(qq{
+<h1>Stack</h1>
+<pre>
+$stack_dump
+</pre>
+<h1>Cycles</h1>
+<pre>
+$cycles
+</pre>
+<h1>Object</h1>
+<pre>
+$obj_dump
+</pre>
+        });
+}
+
+my %shortnames;
+my $new_shortname = "A";
+
+sub _ref_shortname {
+    my $ref = shift;
+    my $refstr = "$ref";
+    my $refdisp = $shortnames{ $refstr };
+    if ( !$refdisp ) {
+        my $sigil = ref($ref) . " ";
+        $sigil = '%' if $sigil eq "HASH ";
+        $sigil = '@' if $sigil eq "ARRAY ";
+        $sigil = '$' if $sigil eq "REF ";
+        $sigil = '&' if $sigil eq "CODE ";
+        $refdisp = $shortnames{ $refstr } = $sigil . $new_shortname++;
+    }
+
+    return $refdisp;
+}
+
+
+
+
+sub request : Local {
+    my ( $self, $c, $request_id ) = @_;
+
+    my $log = $c->devel_events_log;
+
+    my $trackers = $c->object_tracker_hash;
+
+    my @events = $log->limit( from => { request_id => $request_id }, to => "request_end" );
+
+    use YAML::Syck qw/Dump/;
+
+    my $log_output = Dump(@events);
+
+    my @leaks = map {
+        my %rec = %$_;
+        $rec{size} = Devel::Size::total_size($_->{object});
+        $rec{class} = ref($_->{object});
+        \%rec,
+    } sort { $a->{id} <=> $b->{id} } values %{ $trackers->{$request_id}->live_objects };
+
+
+    my @fields = qw/id size class/;
+
+    my %fmt = map { $_ => sub { $_[0] } } @fields;
+
+    $fmt{id} = sub {
+        my $id = shift;
+        return sprintf q{<a href="%s">%s</a>}, $c->uri_for("/object/$request_id/$id"), $id;
+    };
+
+    $fmt{size} = sub {
+        use Number::Bytes::Human;
+        my $h = Number::Bytes::Human->new;
+        $h->set_options(zero => '-');
+        $h->format(shift);
+    };
+
+    my $leaks = join "\n",
+        q{<table border="1" style="border: 1px solid black; padding: 0.3em">},
+            join('', "<tr>", ( map { qq{<th style="padding: 0.2em">$_</th>} } @fields ), "</tr>"),
+            ( map { my $leak = $_;
+                join ( '', "<tr>",
+                    ( map { '<td style="padding: 0.2em">' . $fmt{$_}->($leak->{$_}) . "</td>" } @fields ),
+                "</tr>" );
+            } @leaks ),
+        "</table>";
+
 
     $c->res->content_type("text/html");
 
